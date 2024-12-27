672e3a503703c98bb6c1f1059ffd17fa292fb8a0
Filip Greiff <filip.greiff@live.se>
2024-12-27 15:38:53 +0100

    inital commit

---
+++ resources.js
@@ -0,0 +1,84 @@
+var resources_Common={
+'emsg_EnterValidEmail' : 'Ange en korrekt e-postadress.',
+'emsg_FieldMandatory' : 'Det här fältet måste fyllas i.',
+'emsg_CharactersAllowedPart2' : 'tecken.',
+'emsg_CharactersAllowedPart1' : 'Ange inte mer än',
+'emsg_CharactersNeeded' : 'Ange minst',
+'emsg_ValidPhoneNo' : 'Giltigt telefonnr innehåller endast siffror, + och -',
+'emsg_FormatForIdNo' : 'Ange korrekt format.',
+'emsg_CheckErrors' : 'Alla fält är inte korrekt ifyllda, se markering.',
+'emsg_AuthorizationError' : 'Ett fel inträffade, det kan bero på inaktivitet. Försök igen med att uppdatera sidan. ',
+'imsg_Loading' : 'Laddar, var god vänta..',
+'imsg_header_Warning' : 'Varning',
+'emsg_DigitsAndHyphen' : 'Enbart siffror och bindestreck är tillåtet.',
+'emsg_EnterOnlyDigits' : 'Ange enbart siffror.',
+'btn_OK' : 'OK',
+'btn_Yes' : 'Ja',
+'btn_Delete' : 'Ta bort',
+'btn_Cancel' : 'Avbryt',
+'btn_No' : 'Nej',
+'imsg_hdr_Information' : 'Information',
+'emsg_ValidPhoneNoStrict' : 'Det angivna numret måste börja med + följt av landskod och telefonnummer.',
+'hdr_Error' : 'Fel',
+'emsg_AnErrorHasOccured' : 'Ett fel uppstod.',
+'hdr_invalidOrganisationalNumber' : 'Organisationsnumret är ogiltigt',
+'hdr_invalidPersonNumber' : 'Ogiltigt personnummer',
+'qmsg_hdr_confirm' : 'Bekräfta',
+'imsg_SessionTimeLeft' : 'Din session upphör om:'
+}
+var resources_F5={
+'Common_Link_ReadLess' : 'Dölj information',
+'Common_Link_ReadMore' : 'Läs mer',
+'Common_Validation_Text_Email_MatchAdministrators_Email' : 'E-postadressen får inte vara samma som den du registreat hos betalleverantören. Försök igen med en annan e-postadress.',
+'Common_Validation_Text_MaxValue' : 'Ange ett värde som är mindre än eller lika med {0}',
+'Common_Validation_Text_MinValue' : 'Ange ett värde som är större än eller lika med {0}',
+'Common_Validation_Text_Unique' : 'Värdet finns redan, ange ett annat värde.',
+'Dialog_Common_Validation_Number' : 'Ange ett korrekt värde, använd enbart siffror.'
+}
+var resources_Site={
+'lbl_Of' : 'av',
+'lbl_Image' : 'Bild',
+'ArticleSearch_lbl_ArticleNo' : 'Art.nr  ',
+'text_Cart_Article' : 'artikel',
+'text_Cart_Articles' : 'artiklar',
+'imsg_hdr_Information' : 'Information',
+'text_NotAllArticlesAddedToCart' : 'Följande artiklar går inte att beställa:',
+'text_AddedArticlesToCart1' : 'av',
+'text_AddedArticlesToCart2' : 'kommer att beställas',
+'imsg_OnlyPublishedVersion' : 'Denna funktionalitet finns endast i publicerad version',
+'text_IEBannerMessage' : 'Internet Explorer stöds inte på denna websidan. Vissa funktioner kanske inte fungerar korrekt om du inte byter till en annan webbläsare.'
+}
+var resources_E_Article_Site={
+'imsg_QuantityExceedsStockBalance' : 'Önskat antal överskrider lagersaldot.',
+'imsg_WellDone' : 'Tack!',
+'imsg_YourReviewSubmitted' : 'Din recension har skickats.',
+'imsg_YourReviewSubmittedWaitingForApproval' : 'Din recension har skickats och väntar på ett godkännande.',
+'imsg_ErrorWhenSubmittingReview' : 'Något gick fel när recensionen skickades.',
+'imsg_CantSubmitMultipleReviews' : 'Du har redan skickat en recension för denna produkten',
+'link_ShowLess' : 'visa färre',
+'link_ShowMore' : 'visa mer',
+'btn_Article_Buy_NoStock' : 'Ej i lager',
+'btn_Article_Buy' : 'Köp'
+}
+var resources_Checkout={
+'lbl_UnavailablePrice' : 'dolda',
+'imsg_SessionExpired' : 'Du har blivit utloggad på grund av för lång inaktivitet. Du kommer att skickas till startsidan.'
+}
+var resources_ShopSettings={
+'imsg_ArticleIsHidden' : 'Artikeln kommer nu att döljas för dina besökare.'
+}
+var resources_CustomerPortal={
+'imsg_CustomerLogOut_text' : 'Här klickar dina kunder för att logga ut från din kundportal.'
+}
+var resources_Cookies={
+'text_CookieConsentUsageInformation' : 'Denna sidan använder cookies för att förbättra din upplevelse.',
+'btn_CookieConsentOk' : 'OK',
+'lbl_CookieConsentMoreInfo' : 'Läs mer här.'
+}
+var resources_VisitorAccount={
+'VisitorMessage_text_ChangePassword' : 'Vi har förbättrat vår inloggning och därför rekommenderar vi dig att uppdatera ditt lösenord i samband med detta.',
+'VisitorAccount_text_ConfirmAccountDeletion' : 'Är du säker på att du vill ta bort ditt konto?'
+}
+var resources_Article_register={
+'tab_btn_delete_file' : 'Radera fil'
+}
